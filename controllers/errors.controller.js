e340f8b23aa2fb3fd16429f180e18810350ebb24
P-Copley <pkcopley@gmail.com>
2023-10-02 20:17:16 +0100

    add else blocks to error handlers

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,17 +1,19 @@
 exports.handlePSQLErrors = (err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ message: "bad request!" });
+  if (err.code === '22P02') {
+    res.status(400).send({ message: 'bad request!' });
+  } else {
+    next(err);
   }
-  next(err);
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ message: err.message });
+  } else {
+    next(err);
   }
-  next(err);
 };
 
 exports.handle500Errors = (err, req, res, next) => {
-  res.status(500).send({ message: "internal server error!" });
+  res.status(500).send({ message: 'internal server error!' });
 };
