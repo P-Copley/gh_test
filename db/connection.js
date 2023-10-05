efe2ef313da4b999f07b9f9e7801911e7ae346e1
P-Copley <pkcopley@gmail.com>
2023-10-05 09:28:12 +0100

    configure app for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,23 @@
-const { Pool } = require("pg");
-const dotenv = require("dotenv");
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || 'development';
 
 dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set!");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set!');
 }
 
 console.log(process.env.PGDATABASE);
 
-const db = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const db = new Pool(config);
 
 module.exports = db;
